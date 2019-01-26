02bd435d6cecfae9832bd9c3bcc55c0346b7d0a1
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-01-26 22:18:15 +0300

    new cleanup tool

---
+++ cleanup/src/main.rs
@@ -0,0 +1,50 @@
+use std::{env, ffi::CString, fs};
+#[macro_use]
+extern crate structopt;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Argv {
+    #[structopt(long = "cgroupfs", short = "c", default_value = "/sys/fs/cgroup")]
+    cgroupfs: String,
+
+    #[structopt(long = "root", short = "r")]
+    root: String,
+
+    #[structopt(long = "jail", short = "j")]
+    jail_id: String,
+}
+
+fn main() {
+    let argv: Argv = Argv::from_args();
+    println!("----> Procfs");
+    let procfs_path = format!("{}/proc", &argv.root);
+    let self_exe = format!("{}/self/exe", &procfs_path);
+    let should_unmount = match fs::File::open(self_exe) {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+    if should_unmount {
+        println!("Ok: procfs is not mounted");
+    } else {
+        println!("Unmounting");
+        let procfs_path = CString::new(procfs_path).unwrap();
+        unsafe {
+            if libc::umount2(procfs_path.as_ptr(), libc::MNT_DETACH) == -1 {
+                let err = nix::errno::errno();
+                let err = nix::errno::from_i32(err);
+                eprintln!("Error while unmounting procfs: {:?}", err);
+            }
+        }
+        println!("done");
+    }
+    println!("----> Cgroups");
+    for subsys in &["pids", "memory", "cpuacct"] {
+        let path = format!("{}/{}/jjs/g-{}", &argv.cgroupfs, subsys, argv.jail_id);
+        println!("deleting {}",&path);
+        if let Err(e) = fs::remove_dir(path) {
+            eprintln!("Error: {:?}", e);
+        }
+    }
+
+}
--- scripts/pkg.ps1
+++ scripts/pkg.ps1
@@ -1,7 +1,12 @@
 #!/usr/bin/env powershell
-Write-Host "building minion_cli"
-cargo build --release --target x86_64-unknown-linux-musl --features dist
+Write-Host "----> building minion_cli" -ForegroundColor Green
+cargo build --bin minion_cli --release --target x86_64-unknown-linux-musl --features dist
+Write-Host "----> building jjs-cleanup" -ForegroundColor Green
+cargo build --bin cleanup --release --target x86_64-unknown-linux-musl
+Write-Host "----> packaging" -ForegroundColor Green
 Set-Location ./pkg
-mkdir minion_cli
-Copy-Item -Path ../target/x86_64-unknown-linux-musl/release/minion_cli -Destination ./minion_cli/minion_cli
-tar -cvzf ./minion_cli.tgz ./minion_cli
+mkdir ar_data
+Copy-Item -Path ../target/x86_64-unknown-linux-musl/release/minion_cli -Destination ./ar_data/minion_cli
+mkdir cleanup
+Copy-Item -Path ../target/x86_64-unknown-linux-musl/release/cleanup -Destination ./ar_data/cleanup
+tar cvzf ./jjs.tgz ./ar_data/

942e1e82df1604d4b4aa673a72808edcae83d096
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-01-29 19:38:50 +0300

    updates

--- libminion-ffi/build.rs
+++ libminion-ffi/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let crate_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let out_file = format!("./bindings.h");
+    let out_file = format!("../../target/minion-ffi.h"); //TODO better path
     cbindgen::generate(crate_root)
         .expect("was unable to generate bindings")
         .write_to_file(out_file);

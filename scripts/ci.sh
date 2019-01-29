bebea8707a7c585fbc81d458d5f144495f287ce2
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-01-29 21:21:15 +0300

    Hack up ci.sh script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 rustup component add clippy
 #RUSTFLAGS="-D warnings" cargo check --all
-cargo clippy -- -D clippy::all -D warnings
+cargo clippy -- -D clippy::all -D warnings \
+    -A renamed-and-removed-lints #this option is workaround (see https://issues.apache.org/jira/browse/THRIFT-4764)

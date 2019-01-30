d83ecc5e710f02ebcb4b1a7640e1d4cd80793178
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-01-30 17:35:41 +0300

    [ci] fix ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rustup component add clippy
+rustup target add x86_64-unknown-linux-musl
 #RUSTFLAGS="-D warnings" cargo check --all
 cargo clippy -- -D clippy::all -D warnings \
     -A renamed-and-removed-lints #this option is workaround (see https://issues.apache.org/jira/browse/THRIFT-4764)

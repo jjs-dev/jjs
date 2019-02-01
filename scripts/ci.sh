f96acaf9e0c87cbdc887194e15a42bbd0525ad04
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-02-01 16:15:06 +0300

    [ci] show backtrace

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+export RUST_BACKTRACE=1
 cargo clippy -- -D clippy::all -D warnings \
     -A renamed-and-removed-lints #this option is workaround (see https://issues.apache.org/jira/browse/THRIFT-4764)
 cd devtool

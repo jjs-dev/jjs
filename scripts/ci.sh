b98952197d049fa144dd24bfa6e71c4bb4a24dd9
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-02-02 14:56:19 +0300

    hack

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-RUN rustup default nightly-2019-01-26
-RUN rustup component add clippy
-RUN rustup target add x86_64-unknown-linux-musl
+rustup default nightly-2019-01-26
+rustup component add clippy
+rustup target add x86_64-unknown-linux-musl
 export RUST_BACKTRACE=1
 cargo clippy -- -D clippy::all -D warnings \
     -A renamed-and-removed-lints #this option is workaround (see https://issues.apache.org/jira/browse/THRIFT-4764)

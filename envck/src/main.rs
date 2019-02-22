6ad46280ab3fafd64bc62a4d38df38000f6195ab
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-02-22 22:57:56 +0300

    envck

---
+++ envck/src/main.rs
@@ -0,0 +1,36 @@
+use std::process::exit;
+use std::fs;
+use std::io::ErrorKind;
+fn main() {
+    println!("checking user");
+    {
+        let uid = unsafe {libc::getuid()};
+        if uid != 0 {
+            eprintln!("ERROR: must be run as root");
+            exit(1);
+        }
+    }
+    println!("checking cgroupfs");
+    {
+        let items = match fs::read_dir("/sys/fs/cgroup") {
+            Ok(x) => x,
+            Err(e) => match e.kind() {
+                ErrorKind::NotFound => {
+                    eprintln!("ERROR: /sys/fs/cgroup not found");
+                    exit(1);
+                }
+                _ => {
+                    eprintln!("ERROR: {:?}", e);
+                    exit(1);
+                }
+            }
+        };
+        let items:Vec<_> = items.map(|x| x.unwrap().file_name().into_string().unwrap()).collect();
+        for subsys in ["pids", "cpuacct", "memory"].iter() {
+            if !items.contains(&String::from(*subsys)) {
+                eprintln!("ERROR: subsystem {} not found", subsys);
+                exit(1);
+            }
+        }
+    }
+}

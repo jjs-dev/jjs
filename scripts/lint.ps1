9c450cf462ce254aa77e2309c1e68bdd14869d54
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-05-10 19:54:15 +0300

    [lint] fail on error

--- scripts/lint.ps1
+++ scripts/lint.ps1
@@ -2,13 +2,26 @@
 param(
     [switch]$Touch
 )
+Set-StrictMode -Version Latest
 $env:RUST_BACKTRACE="1"
 cargo fmt --verbose --all -- --check
+if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+}
 if ($Touch) {
     cargo run -p devtool -- touch --verbose
 }
 cargo clippy --all -- -D clippy::all -D warnings
+if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+}
 New-Item -Path ./minion-ffi/example-c/cmake-build-debug -ItemType Directory -ErrorAction SilentlyContinue
 Set-Location ./minion-ffi/example-c/cmake-build-debug
 cmake ..
+if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+} 
 cmake --build .
+if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+}

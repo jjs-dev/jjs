456fd983d7a135b2eca22ab637b6f3ac3e9c3f36
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-05-14 20:18:39 +0300

    restore changes from 'invoker-implement-launch'

--- invoker/src/invoker.rs
+++ invoker/src/invoker.rs
@@ -1,11 +1,29 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum StatusKind {
-    //Rejected,
+    /// WA, TLE, rejected by teacher, etc
+    Rejected,
     /// e.g. Coding Style Violation
     CompilationError,
-    //Partial,
-    //Accepted,
+    // Evaluated,
+    Accepted,
     NotSet,
+    InternalError,
+}
+
+pub mod status_codes {
+    macro_rules! declare_code {
+        ($code: ident) => {
+            pub const $code: &str = stringify!($code);
+        };
+
+        ($code: ident, $($codes: ident),+) => {
+             declare_code!($code);
+             declare_code!($($codes),+);
+        };
+    }
+
+    declare_code!(TOOLCHAIN_SEARCH_ERROR, BUILT, COMPILATION_TIMED_OUT, COMPILER_FAILED);
+    declare_code!(TIME_LIMIT_EXCEEDED, RUNTIME_ERROR, TEST_PASSED);
 }
 
 #[derive(Debug)]
@@ -13,10 +31,3 @@ pub struct Status {
     pub kind: StatusKind,
     pub code: String,
 }
-/*
-#[derive(Debug)]
-pub struct Limits {
-    pub memory: u64,
-    pub time: std::time::Duration,
-}
-*/

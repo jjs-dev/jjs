e73778bac99748df49574c45a79cdd71444cf5df
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-10-10 14:56:29 +0300

    Updates for CI

--- scripts/ci-env.sh
+++ scripts/ci-env.sh
@@ -4,7 +4,7 @@ bash src/deploy/deps.sh
 wget -q -O - https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-Linux-x86_64.sh > /tmp/cmake.sh
 sudo bash /tmp/cmake.sh --skip-license --prefix=/usr
 sudo rm -rf /usr/local/bin/cmake # this is hack to make script use new cmake version
-if [ "$SECRET_ENABLED" = "1" ]; then
+if [[ "x$SECRET_ENABLED" != "x" ]]; then
     wget -q -O - https://files.viva64.com/etc/pubkey.txt | sudo apt-key add -;
     sudo wget -O /etc/apt/sources.list.d/viva64.list https://files.viva64.com/etc/viva64.list;
     sudo apt-get update;

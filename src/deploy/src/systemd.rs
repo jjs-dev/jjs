c6fdd3157bc35259e8c9605365f6aedf55877ab6
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-12-16 16:13:01 +0300

    Update tera usage at deploy/systemd

--- src/deploy/src/systemd.rs
+++ src/deploy/src/systemd.rs
@@ -1,23 +1,21 @@
 //! This module implements support for systemd config generation
 //! Templates are in systemd/*, we just want to compile them and put to appropriate place
 use serde::Serialize;
-use tera::compile_templates;
 
 #[derive(Serialize)]
-struct TplCtxt {
+struct TplCtx {
     jjs_sysroot: String,
 }
 
 pub(crate) fn build(params: &crate::Params) {
     if let Some(install_prefix) = &params.install_prefix {
-        let tpls = compile_templates!("systemd/**/*");
-        let tplcx = TplCtxt {
-            jjs_sysroot: install_prefix.display().to_string(),
-        };
+        let tpls = tera::Tera::new("systemd/**/*").expect("failed to compile tera templates");
+        let mut tpl_render_ctx = tera::Context::new();
+        tpl_render_ctx.insert("jjs_sysroot", &install_prefix.display().to_string());
         let emit_unit = |unit_name| {
             let unit_tpl_name = format!("jjs-{}.service.tera", unit_name);
             let unit_text = tpls
-                .render(&unit_tpl_name, &tplcx)
+                .render(&unit_tpl_name, &tpl_render_ctx)
                 .expect("unit interpolation failed");
             let out_path = params
                 .artifacts

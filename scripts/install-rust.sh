26977d1d615fbab782c129791fdf4c7caa15f45f
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-01-17 13:06:02 +0300

    Pin rust version

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-rustup toolchain install nightly -c clippy -c rustfmt
+# TODO: unpin rust toolchain when https://github.com/rust-lang/rust/issues/68264 is fixed
+# rustup toolchain install nightly -c clippy -c rustfmt
+rustup toolchain install nightly-2010-01-07

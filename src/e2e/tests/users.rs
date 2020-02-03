ff7a3a5688c5083855afe6a3bd8da875e8735585
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-02-03 23:23:15 +0300

    Add tests for weird logins and passwords

---
+++ src/e2e/tests/users.rs
@@ -0,0 +1,36 @@
+fn check_login_and_password(login: &str, password: &str) {
+    e2e::RequestBuilder::new()
+        .operation(
+            r#"
+mutation CreateUser($login: String!, $password: String!) {
+    createUser(login: $login, password: $password, groups: []) {
+        id
+    }
+}
+"#,
+        )
+        .var("login", &login.into())
+        .var("password", &password.into())
+        .exec()
+        .unwrap_ok();
+    e2e::RequestBuilder::new()
+        .operation(
+            r#"
+mutation LogIn($login: String!, $password: String!) {
+    authSimple(login: $login, password: $password) {
+        data
+    }
+}
+"#,
+        )
+        .var("login", &login.into())
+        .var("password", &password.into())
+        .exec()
+        .unwrap_ok();
+}
+
+#[test]
+fn test_unicode() {
+    check_login_and_password("猫鯉", "ありがとうございまので大丈夫");
+    check_login_and_password("💻🌐", "🔑");
+}

1cf112f04b2ad7e6691d2875ab603d51be9fe683
Sergey Lisov <sleirsgoevy@gmail.com>
2020-03-08 18:49:13 +0300

    vm-sysroot: image/build-*.sh: de-hardcode paths

--- vm-sysroot/image/build-full.sh
+++ vm-sysroot/image/build-full.sh
@@ -2,23 +2,57 @@
 
 set -e
 
-if [ "$$" != 1 ] && [ "x$1" != x ]
-then cat >&2 << EOF
-usage: image/build-full.sh
+usage ()
+{
+    cat >&2 << EOF
+usage: image/build-full.sh [--out <out_path>] [--sysroot <sysroot_path>]
 
-Build the disk image image/full.img, using sysroot in ./sysroot.
+Build the disk image \$out_path (default: image/full.img), using sysroot in \$sysroot_path (default: ./sysroot).
 Unlike image/build-image.sh, the resulting image is a ready-to-boot raw disk image.
 This script must be run as root in order to function properly.
 EOF
-exit 1
-fi
+    exit 1
+}
 
-cd "$(dirname "$0")"
-bash ./build-image.sh
+dir="$(dirname "$0")"
+out="$dir/full.img"
+sysroot="$dir/../sysroot"
 
-dd if=/dev/null of=full.img
-dd if=/dev/null of=full.img bs=1048576 seek=1025
-fdisk full.img << EOF
+while [ "x$1" != x ]
+do
+    if [ "x$1" == x--out ]
+    then
+        out="$2"
+        shift; shift
+    elif [ "x$1" == x--sysroot ]
+    then
+        sysroot="$2"
+        shift; shift
+    else
+        usage
+    fi
+done
+
+hdd_img="$(mktemp)"
+
+abspath ()
+{
+    local x="$1"
+    if [ "${x:0:1}" == "/" ]
+    then echo "$x"
+    else echo "$(pwd)/$x"
+    fi
+}
+
+dir="$(abspath "$dir")"
+out="$(abspath "$out")"
+sysroot="$(abspath "$sysroot")"
+
+bash "$dir/build-image.sh" --out "$hdd_img" --sysroot "$sysroot"
+
+dd if=/dev/null of="$out"
+dd if=/dev/null of="$out" bs=1048576 seek=1025
+fdisk "$out" << EOF
 n
 p
 1
@@ -26,15 +60,19 @@ p
 
 w
 EOF
-dd if=/dev/null of=full.img bs=1048576 seek=1
+dd if=/dev/null of="$out" bs=1048576 seek=1
 
+tmp_dir="$(mktemp -d)"
+(
+cd "$tmp_dir"
 mkdir tmpfs #this is intentional, don't rename
 mount -t tmpfs tmpfs tmpfs
 cp /usr/lib/grub/i386-pc/boot.img tmpfs/
 grub-mkimage -O i386-pc -o tmpfs/core.img -p '(hd0,1)/boot/grub' biosdisk part_msdos ext2 linux normal
-echo '(hd0) ./full.img' > tmpfs/dmap.txt
-grub-bios-setup --device-map tmpfs/dmap.txt -d tmpfs -s full.img
+echo "(hd0) $out" > tmpfs/dmap.txt
+grub-bios-setup --device-map tmpfs/dmap.txt -d tmpfs -s "$out"
 umount tmpfs
 rmdir tmpfs
-cat hdd.img >> full.img
-
+cat "$hdd_img" >> "$out"
+)
+rmdir "$tmp_dir"
--- vm-sysroot/image/build-image.sh
+++ vm-sysroot/image/build-image.sh
@@ -2,20 +2,38 @@
 
 set -e
 
-if [ "$$" != 1 ] && [ "x$1" != x ]
-then cat >&2 << EOF
-usage: image/build-image.sh
+usage ()
+{
+    cat >&2 << EOF
+usage: image/build-image.sh [--out <output_path>] [--sysroot <sysroot_path>]
 
-Build the disk image image/hdd.img, using sysroot in ./sysroot.
+Build the disk image \$output_path (default: image/hdd.img), using sysroot in \$sysroot_path (default: ./sysroot).
 The resulting image is a single partition without any bootloader/kernel/whatsoever.
 This script must be run as root in order to function properly.
 EOF
-exit 1
-fi
+    exit 1
+}
 
-cd "$(dirname "$0")"
+dir="$(dirname "$0")"
+out="$dir/hdd.img"
+sysroot="$dir/../sysroot"
 
-dd if=/dev/null of=hdd.img
-dd if=/dev/null of=hdd.img bs=1048576 seek=1024
-mke2fs -d ../sysroot hdd.img
+while [ "x$1" != x ]
+do
+    if [ "x$1" == x--out ]
+    then
+        out="$2"
+        shift; shift
+    elif [ "x$1" == x--sysroot ]
+    then
+        sysroot="$2"
+        shift; shift
+    else
+        usage
+    fi
+done
+
+dd if=/dev/null of="$out"
+dd if=/dev/null of="$out" bs=1048576 seek=1024
+mke2fs -d "$sysroot" "$out"
 

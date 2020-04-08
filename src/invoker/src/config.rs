95cc15d0818c996d852e95571a8a33f760f5500d
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-04-08 23:05:27 +0300

    Allow sleep section omission

--- src/invoker/src/config.rs
+++ src/invoker/src/config.rs
@@ -4,8 +4,10 @@ use serde::{Deserialize, Serialize};
 pub struct InvokerConfig {
     /// How many workers should be spawned
     /// By default equal to processor count
+    #[serde(default)]
     pub workers: Option<usize>,
     /// Configures how much invoker will sleep between ticks
+    #[serde(default)]
     pub sleep: SleepConfig,
 }
 #[derive(Serialize, Deserialize)]
@@ -25,5 +27,14 @@ impl SleepConfig {
 
     fn default_step() -> u32 {
         500
+    }
+}
+
+impl Default for SleepConfig {
+    fn default() -> Self {
+        SleepConfig {
+            max_ms: Self::default_max(),
+            step_ms: Self::default_step(),
+        }
     }
 }

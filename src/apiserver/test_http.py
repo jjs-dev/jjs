8d21b73f36c242489fca3a4b39904ab42ac7072a
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-05-18 20:54:45 +0300

    Add smoke tests

---
+++ src/apiserver/test_http.py
@@ -0,0 +1,13 @@
+from .main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_smoke_is_dev():
+    res = client.get("/system/is-dev")
+    assert res.status_code == 200
+
+
+def test_smoke_api_version():
+    res = client.get("/system/api-version")
+    assert res.status_code == 200

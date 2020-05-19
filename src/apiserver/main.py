62717f5926e7ec114379c9d3fc06a92973804c84
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-05-20 00:34:19 +0300

    Support run submits

--- src/apiserver/main.py
+++ src/apiserver/main.py
@@ -2,8 +2,21 @@ import fastapi
 from pydantic import BaseModel
 import os
 import json
+import typing
+import uuid
+import pymongo
+import urllib.parse
+import functools
 
 app = fastapi.FastAPI()
+
+
+@functools.lru_cache
+def db_connect() -> pymongo.database.Database:
+    db_url = os.environ["DATABASE_URL"]
+    client = pymongo.MongoClient(db_url)
+    db_name = urllib.parse.urlparse(db_url).path.replace('/', '')
+    return client[db_name]
 
 
 @app.get('/system/is-dev', response_model=bool, operation_id="isDev")
@@ -37,6 +50,53 @@ def route_api_version():
     should assert that MAJOR = X and MINOR >= Y
     """
     return ApiVersion(major=0, minor=0)
+
+
+class Run(BaseModel):
+    id: uuid.UUID
+    """
+    Run identifier, unique in the system.
+    """
+    toolchain_id: str
+    problem_id: str
+    user_id: uuid.UUID
+    contest_id: str
+
+
+class RunSubmitSimpleParams(BaseModel):
+    code: str
+    """
+    Base64-encoded source text
+    """
+    contest: str
+    """
+    Contest where run is submitted
+    """
+    problem: str
+    """
+    Problem name, relative to contest
+    """
+    toolchain: str
+    """
+    Toolchain to use when judging this run
+    """
+
+
+@app.post('/runs', response_model=Run, operation_id="submitRun")
+def route_submit(params: RunSubmitSimpleParams):
+    """
+    Submits new run
+
+    This operation creates new run, with given source code, and queues it for
+    judging. Created run will be returned. All fields against `id` will match
+    fields of request body; `id` will be real id of this run.
+    """
+    run_uuid = uuid.uuid4()
+    user_id = uuid.UUID('12345678123456781234567812345678')
+    r = Run(id=run_uuid, toolchain_id=params.toolchain,
+            problem_id=params.problem, user_id=user_id, contest_id=params.contest_id)
+    db = db_connect()
+    db.runs.insert_one(r)
 
 
 if os.environ.get("__JJS_SPEC") is not None:

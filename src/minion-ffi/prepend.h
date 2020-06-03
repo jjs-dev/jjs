6462217772936e00a8a0d766ff77abc28a1f66c0
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-06-04 01:26:30 +0300

    Remove sources

--- src/minion-ffi/prepend.h
+++
@@ -1,7 +0,0 @@
-#if defined(__GNUC__) || defined(__clang__)
-#define ATTR_MUST_USE __attribute__((warn_unused_result))
-#elif defined(_MSC_VER) && (_MSC_VER >= 1700)
-#define ATTR_MUST_USE _Check_result
-#else
-#define ATTR_MUST_USE
-#endif

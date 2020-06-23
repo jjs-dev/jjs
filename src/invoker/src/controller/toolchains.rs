a48b42f6ceaba81b034be9ff7f198df3fa2aa00e
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-06-23 18:53:20 +0300

    Download images without Docker

--- src/invoker/src/controller/toolchains.rs
+++ src/invoker/src/controller/toolchains.rs
@@ -58,95 +58,19 @@ pub struct ResolvedToolchain {
     pub path: PathBuf,
 }
 
-struct ImageManager {
-    /// Which container manager to use - docker/podman
-    manager: String,
-}
-
-async fn binary_exists(binary_name: &str) -> anyhow::Result<bool> {
-    let mut cmd = tokio::process::Command::new("which");
-    cmd.arg(binary_name);
-    let status = cmd.status().await.context("spawn error")?;
-    Ok(status.success())
-}
-
-impl ImageManager {
-    async fn new() -> anyhow::Result<ImageManager> {
-        if let Ok(hint) = std::env::var("JJS_INVOKER_IMAGE_MANAGER") {
-            return Ok(ImageManager { manager: hint });
-        }
-        if binary_exists("podman").await? {
-            return Ok(ImageManager {
-                manager: "podman".to_string(),
-            });
-        }
-        if binary_exists("docker").await? {
-            return Ok(ImageManager {
-                manager: "docker".to_string(),
-            });
-        }
-        anyhow::bail!("can not find image manager")
-    }
-
-    async fn save(&self, image_name: &str, target_dir: &Path) -> anyhow::Result<()> {
-        let container_id = {
-            let mut spawn_cmd = tokio::process::Command::new(&self.manager);
-            spawn_cmd.arg("create");
-            spawn_cmd.arg(image_name);
-            let spawn_out = spawn_cmd.output().await.context("process spawn error")?;
-            if !spawn_out.status.success() {
-                eprintln!("stdout:\n{}", String::from_utf8_lossy(&spawn_out.stdout));
-                eprintln!("stderr:\n{}", String::from_utf8_lossy(&spawn_out.stderr));
-                anyhow::bail!("container create failed: exit code {:?}", spawn_out.status);
-            }
-            let container_id =
-                String::from_utf8(spawn_out.stdout).context("corrupted create output")?;
-
-            container_id.trim().to_string()
-        };
-        {
-            let mut copy_cmd = tokio::process::Command::new(&self.manager);
-            copy_cmd.arg("copy");
-            // copy all filesystem of container...
-            copy_cmd.arg(format!("{}:/", &container_id));
-            // ...to `target_dir`
-            copy_cmd.arg(target_dir);
-            let copy_out = copy_cmd.output().await.context("process spawn error")?;
-            if !copy_out.status.success() {
-                eprintln!("stdout:\n{}", String::from_utf8_lossy(&copy_out.stdout));
-                eprintln!("stderr:\n{}", String::from_utf8_lossy(&copy_out.stderr));
-                anyhow::bail!("copy failed: exit code {:?}", copy_out.status);
-            }
-        }
-        {
-            let mut kill_cmd = tokio::process::Command::new(&self.manager);
-            kill_cmd.arg("rm");
-            kill_cmd.arg(container_id);
-            let kill_out = kill_cmd.output().await.context("process spawn error")?;
-            if !kill_out.status.success() {
-                eprintln!("stdout:\n{}", String::from_utf8_lossy(&kill_out.stdout));
-                eprintln!("stderr:\n{}", String::from_utf8_lossy(&kill_out.stderr));
-                anyhow::bail!("kill failed: exit code {:?}", kill_out.status);
-            }
-        }
-
-        Ok(())
-    }
-}
-
 /// Responsible for fetching toolchains
 pub struct ToolchainLoader {
-    image_manager: ImageManager,
+    puller: puller::Puller,
     toolchains_dir: tempfile::TempDir,
 }
 
 impl ToolchainLoader {
     pub async fn new() -> anyhow::Result<ToolchainLoader> {
-        let image_manager = ImageManager::new().await?;
+        let puller = puller::Puller::new().await;
         let toolchains_dir = tempfile::TempDir::new()?;
         Ok(ToolchainLoader {
-            image_manager,
             toolchains_dir,
+            puller,
         })
     }
 
@@ -159,10 +83,15 @@ impl ToolchainLoader {
         tokio::fs::create_dir(target_dir)
             .await
             .context("failed to create target dir")?;
-        self.image_manager
-            .save(toolchain_url, target_dir)
+
+        self.puller
+            .pull(
+                toolchain_url,
+                target_dir,
+                tokio::sync::CancellationToken::new(),
+            )
             .await
-            .context("failed to save toolchain")?;
+            .context("failed to pull toolchain")?;
         Ok(())
     }
 

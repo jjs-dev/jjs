453b5183d3bdf6c7f20d7bd96c2765659e28cd24
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-10-31 23:29:29 +0300

    work

--- src/invoker/src/main.rs
+++ src/invoker/src/main.rs
@@ -1,3 +1,12 @@
+struct RpcHandler;
+
+impl rpc::Handler for RpcHandler {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = rpc::RouterBuilder::new();
+    server.add_route(RpcHandler);
+    let server = server.build().as_make_service();
+    let server = hyper
 }

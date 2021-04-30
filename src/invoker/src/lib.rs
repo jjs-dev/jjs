9b9b91229146dba625e0543611e26c0e81ad38c4
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-05-01 01:02:46 +0300

    Exclude components moved to other repos

--- src/invoker/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![type_length_limit = "4323264"]
-pub mod api;
-pub mod config;
-pub mod controller;
-pub mod init;
-mod scheduler;
-pub mod sources;
-pub mod worker;
--- src/invoker/tests/separated_feedback.rs
+++
@@ -1,4 +0,0 @@
-#[tokio::test]
-async fn separated_feedback() {
-    // TODO write this test when cfg is rewritten
-}
